2a1118816d4f34f8bda0c764b6057dbe318c9d1d
Marcos <marcos.artur.f@gmail.com>
2022-10-21 11:37:16 -0300

    Curso CONTROLE_FLUXO concluído

---
+++ Bootcamp_Quebec_Java_Digital/03-Dominando_a_Linguagem_de_Programacao_Java/CONTROLE-FLUXO/src/ExemploForArray.java
@@ -0,0 +1,14 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for(int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no índice x = " + x + " é " + alunos[x]);
+        }
+        System.out.println();
+
+        for(String aluno : alunos) {
+            System.out.println("O nome do aluno é " + aluno);
+        }
+    }
+}
---
+++ Bootcamp_Quebec_Java_Digital/03-Dominando_a_Linguagem_de_Programacao_Java/CONTROLE-FLUXO/src/FormatadorCepExemplo.java
@@ -0,0 +1,18 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            //throw new RuntimeException(e);
+            System.out.println("O CEP informado é inválido.");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+            throw new CepInvalidoException();
+
+        // simulando um cep formatado
+        return "23.765-064";
+    }
+}
