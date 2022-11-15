637d9ce88c270dc82bad9f622814e3de7818c34e
Marcos <marcos.artur.f@gmail.com>
2022-11-15 12:53:02 -0300

    Aula 4 de OO Completa

---
+++ Bootcamp_Quebec_Java_Digital/03-Dominando_a_Linguagem_de_Programacao_Java/OOAula04.2/src/one/digitalinnovation/oo/RodarAplicacao.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.oo;
+
+public class RodarAplicacao {
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario();
+
+        Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Faxineiro();
+
+//         Gerente gerente_ = new Faxineiro();
+//        Vendedor vendedor_ = (Vendedor) new Funcionario();
+    }
+}
